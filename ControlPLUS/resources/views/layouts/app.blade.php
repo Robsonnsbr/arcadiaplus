13f620754a64ddf64cbe598287cb90e080bb13f5
robsonnsbr <robsonnsbr@gmail.com>
2026-02-26 16:03:05 -0300

    fix: ..

--- ControlPLUS/resources/views/layouts/app.blade.php
+++ ControlPLUS/resources/views/layouts/app.blade.php
@@ -641,6 +641,7 @@
         <script src='https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js'></script>
         <script src="/assets/js/app.js"></script>
         <script type="text/javascript" src="/js/main.js"></script>
+        <script type="text/javascript" src="/js/dropdown-table-zindex-fix.js"></script>
         <script src="/assets/vendor/flatpickr/flatpickr.min.js"></script>
         <script src="/assets/js/pages/demo.flatpickr.js"></script>
 
