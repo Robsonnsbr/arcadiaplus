0c867ab18bb85aa8b2c3d4e717ddbcbc0eeda26f
robsonnsbr <robsonnsbr@gmail.com>
2026-04-13 18:01:13 -0300

    feat: add test file

---
+++ ControlPLUS/scripts/simple-test.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Teste rápido do PHP no ambiente (host ou container).
+ *
+ * Uso: php scripts/simple-test.php
+ */
+
+echo 'PHP ' . PHP_VERSION . PHP_EOL;
+echo 'OK' . PHP_EOL;
+
+exit(0);
